303f385e13f26ded8e88d4e91af87f95299c42f7
Shawn Yang <yangshawn14@gmail.com>
2024-05-28 18:46:37 -0600

    Create chat page

---
+++ client/src/Chat.jsx
@@ -0,0 +1,7 @@
+export default function Chat() {
+    return (
+        <div className="flex h-screen">
+            div.bg-blue-100
+        </div>
+    );
+}
--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,13 +1,14 @@
 import { useContext } from "react";
 import RegisterAndLoginForm from "./RegisterAndLoginForm";
 import { UserContext } from "./userContext";
+import Chat from "./Chat";
 
 
 export default function Routes() {
     const { username, id } = useContext(UserContext);
 
     if (username) {
-        return 'logged in!' + username;
+        return <Chat />
     }
 
     return (
