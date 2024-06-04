9e1e9f7184cb75b0f1b2033deff7e497263da792
Shawn Yang <yangshawn14@gmail.com>
2024-06-04 17:38:28 -0600

    Allow chat history

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const user = require('./models/user');
+const Message = require('./models/message');
 const bcrypt = require('bcryptjs');
 const ws = require('ws');
 
@@ -23,12 +24,37 @@ app.use(cors({
     origin: process.env.CLIENT_URL,
 }));
 
+async function getUserDataFromRequest(req) {
+    return new Promise((resolve, reject) => {
+        const token = req.cookies?.token;
+        if (token) {
+            jwt.verify(token, jwtSecret, {}, (err, userData) => {
+                if (err) throw err;
+                resolve(userData);
+            });
+        } else {
+            reject('no token');
+        }
+    });
+}
+
 app.get('/test', (req, res) => {
     res.json('test ok');
 });
 
+app.get('/messages/:userId', async (req, res) => {
+    const { userId } = req.params;
+    const userData = await getUserDataFromRequest(req);
+    const ourUserId = userData.userId;
+    const messages = await Message.find({
+        sender: { $in: [userId, ourUserId] },
+        recipient: { $in: [userId, ourUserId] },
+    }).sort({ createdAt: 1 })
+    res.json(messages);
+});
+
 app.get('/profile', (req, res) => {
-    const { token } = req.cookies;
+    const token = req.cookies?.token;
     if (token) {
         jwt.verify(token, jwtSecret, {}, (err, userData) => {
             if (err) throw err;
@@ -84,8 +110,8 @@ app.post('/register', async (req, res) => {
 const server = app.listen(4040);
 
 const wss = new ws.WebSocketServer({ server });
-
 wss.on('connection', (connection, req) => {
+    // read username and id from the cookie for this connection
     const cookies = req.headers.cookie;
     if (cookies) {
         const tokenCookieString = cookies.split(';').find(str => str.startsWith('token='));
@@ -102,6 +128,26 @@ wss.on('connection', (connection, req) => {
         }
     }
 
+    connection.on('message', async (message) => {
+        const messageData = JSON.parse(message.toString());
+        const { recipient, text } = messageData;
+        if (recipient && text) {
+            const messageDoc = await Message.create({
+                sender: connection.userId,
+                recipient,
+                text,
+            });
+            [...wss.clients]
+                .filter(c => c.userId === recipient)
+                .forEach(c => c.send(JSON.stringify({
+                    text,
+                    sender: connection.userId,
+                    _id: messageDoc._id,
+                })));
+        }
+    });
+
+    // Notify everyone about online people
     [...wss.clients].forEach(client => {
         client.send(JSON.stringify({
             online: [...wss.clients].map(c => ({ userId: c.userId, username: c.username }))
--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,16 +1,32 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import Avatar from "./Avatar";
+import Logo from "./Logo";
+import { UserContext } from "./userContext";
+import { uniqBy } from "lodash";
+import axios from "axios";
 
 export default function Chat() {
     const [ws, setWs] = useState(null);
     const [onlinePeople, setOnlinePeople] = useState({});
     const [selectedUserId, setSelectedUserId] = useState(null);
+    const [newMessageText, setNewMessageText] = useState('');
+    const [messages, setMessages] = useState([]);
+    const { username, id } = useContext(UserContext);
+    const divUnderMessages = useRef();
     useEffect(() => {
+        connectToWs();
+    }, []);
+    function connectToWs() {
         const ws = new WebSocket('ws://localhost:4040');
         setWs(ws);
-        ws.addEventListener('message', handleMessage)
-    }, []);
-
+        ws.addEventListener('message', handleMessage);
+        ws.addEventListener('close', () => {
+            setTimeout(() => {
+                console.log('Disconnected. Trying to reconnect');
+                connectToWs();
+            }, 1000);
+        });
+    }
     function showOnlinePeople(peopleArray) {
         const people = {};
         peopleArray.forEach(({ userId, username }) => {
@@ -21,46 +37,105 @@ export default function Chat() {
 
     function handleMessage(e) {
         const messageData = JSON.parse(e.data);
+        console.log({ e, messageData });
         if ('online' in messageData) {
             showOnlinePeople(messageData.online);
+        } else if ('text' in messageData) {
+            setMessages(prev => ([...prev, { ...messageData }]));
         }
     }
 
+    function sendMessage(e) {
+        e.preventDefault();
+        ws.send(JSON.stringify({
+            recipient: selectedUserId,
+            text: newMessageText,
+        }));
+        setNewMessageText('');
+        setMessages(prev => ([...prev, {
+            text: newMessageText,
+            sender: id,
+            recipient: selectedUserId,
+            _id: Date.now(),
+        }]));
+    }
+    useEffect(() => {
+        const div = divUnderMessages.current;
+        if (div) {
+            div.scrollIntoView({ behavior: 'smooth', block: 'end' });
+        }
+    }, [messages]);
+
+    useEffect(() => {
+        if (selectedUserId) {
+            axios.get('/messages/' + selectedUserId).then(res => {
+                setMessages(res.data);
+            });
+        }
+    }, [selectedUserId]);
+
+    const onlinePeopleExcludingUser = { ...onlinePeople };
+    delete onlinePeopleExcludingUser[id];
+
+    const messagesWithoutDupes = uniqBy(messages, '_id');
+
+
     return (
         <div className="flex h-screen">
-            <div className="bg-white-100 w-1/3 pl-4 pt-4">
-                <div className="text-blue-600 font-bold flex gap-2 mb-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                        <path d="M4.913 2.658c2.075-.27 4.19-.408 6.337-.408 2.147 0 4.262.139 6.337.408 1.922.25 3.291 1.861 3.405 3.727a4.403 4.403 0 0 0-1.032-.211 50.89 50.89 0 0 0-8.42 0c-2.358.196-4.04 2.19-4.04 4.434v4.286a4.47 4.47 0 0 0 2.433 3.984L7.28 21.53A.75.75 0 0 1 6 21v-4.03a48.527 48.527 0 0 1-1.087-.128C2.905 16.58 1.5 14.833 1.5 12.862V6.638c0-1.97 1.405-3.718 3.413-3.979Z" />
-                        <path d="M15.75 7.5c-1.376 0-2.739.057-4.086.169C10.124 7.797 9 9.103 9 10.609v4.285c0 1.507 1.128 2.814 2.67 2.94 1.243.102 2.5.157 3.768.165l2.782 2.781a.75.75 0 0 0 1.28-.53v-2.39l.33-.026c1.542-.125 2.67-1.433 2.67-2.94v-4.286c0-1.505-1.125-2.811-2.664-2.94A49.392 49.392 0 0 0 15.75 7.5Z" />
-                    </svg>
-
-                    ChatApp
-                </div>
-                {Object.keys(onlinePeople).map(userId => (
+            <div className="bg-white-100 w-1/3">
+                <Logo />
+                {Object.keys(onlinePeopleExcludingUser).map(userId => (
                     <div key={userId}
                         onClick={() => setSelectedUserId(userId)}
-                        className={"border-b border-gray-100 py-2 flex gap-2 items-center cursor-pointer " + (userId === selectedUserId ? 'bg-blue-50' : '')}>
-                        <Avatar username={onlinePeople[userId]} userId={userId} />
-                        <span className="text-gray-800">{onlinePeople[userId]}</span>
+                        className={"border-b border-gray-100 flex gap-2 items-center cursor-pointer " + (userId === selectedUserId ? 'bg-blue-50' : '')}>
+                        {userId === selectedUserId && (
+                            <div className="w-1 bg-blue-500 h-12 rounded-r-md"></div>
+                        )}
+                        <div className="flex gap-2 py-2 pl-4 items-center">
+                            <Avatar online={true} username={onlinePeople[userId]} userId={userId} />
+                            <span className="text-gray-800">{onlinePeople[userId]}</span>
+                        </div>
 
                     </div>
                 ))}
             </div>
             <div className="flex flex-col bg-blue-50 w-2/3 p-2">
-                <div className="flex-grow">messages</div>
-                <div className="flex gap-2">
-                    <input type="text"
-                        placeholder="Type your message here"
-                        className="bg-white flex-grow border rounded-sm p-2" />
-                    <button className="bg-blue-500 p-2 text-white rounded-sm">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
-                        </svg>
-
-
-                    </button>
+                <div className="flex-grow">
+                    {!selectedUserId && (
+                        <div className="flex h-full items-center justify-center">
+                            <div className="text-gray-400">&larr; Select a contact from sidebar</div>
+                        </div>
+                    )}
+                    {!!selectedUserId && (
+                        <div className="relative h-full">
+                            <div className="overflow-y-scroll absolute top-0 left-0 right-0 bottom-2">
+                                {messagesWithoutDupes.map(message => (
+                                    <div key={message._id} className={(message.sender === id ? 'text-right' : 'text-left')}>
+                                        <div className={"text-left inline-block p-2 my-2 rounded-md text-sm " + (message.sender === id ? 'bg-blue-500 text-white' : 'bg-white text-gray-500')}>
+                                            {message.text}
+                                        </div>
+                                    </div>
+                                ))}
+                                <div ref={divUnderMessages}></div>
+                            </div>
+                        </div>
+                    )}
                 </div>
+                {!!selectedUserId && (
+                    <form className="flex gap-2" onSubmit={sendMessage}>
+                        <input type="text"
+                            value={newMessageText}
+                            onChange={e => setNewMessageText(e.target.value)}
+                            placeholder="Type your message here"
+                            className="bg-white flex-grow border rounded-sm p-2" />
+                        <button type="submit" className="bg-blue-500 p-2 text-white rounded-sm">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+                            </svg>
+                        </button>
+                    </form>
+                )}
+
             </div>
         </div>
     );
